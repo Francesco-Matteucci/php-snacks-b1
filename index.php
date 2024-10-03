4ad64b3703d25c0f3ca02a59a9c097483b4d7645
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-10-03 13:13:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+
+$posts = [
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack</title>
+</head>
+
+<body>
+
+    <?php foreach ($posts as $date => $postList) { ?>
+    <h3>Post del: <?= $date ?></h3>
+    <ul>
+        <?php foreach ($postList as $post) { ?>
+        <li>
+            <strong>Titolo del post:</strong> <?= $post['title'] ?><br>
+            <strong>Autore del post:</strong> <?= $post['author'] ?><br>
+            <strong>Testo del post:</strong> <?= $post['text'] ?>
+        </li>
+        <?php } ?>
+    </ul>
+    <?php } ?>
+
+</body>
+
+</html>
