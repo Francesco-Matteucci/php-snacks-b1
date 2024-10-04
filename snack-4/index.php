8f4ec19d46ba69f9521cf7c8ccb4333f96b9d416
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-10-04 13:59:46 +0200

    Snack4B - Display students with sufficient grades

--- snack-4/index.php
+++ snack-4/index.php
@@ -17,12 +17,17 @@ require_once __DIR__ . '/classes.php';
     <?php foreach ($classi as $classe => $studenti) { ?>
     <h2><?= $classe ?></h2>
     <ul>
-        <?php foreach ($studenti as $studente) { ?>
-        <li><?= $studente['nome'] . ' ' . $studente['cognome'] ?> ~ Età: <?= $studente['anni'] ?> ~ Votomedio:
+        <?php foreach ($studenti as $studente) {
+            
+                // Effettuo un controllo per vedere se il voto medio di uno studente è sufficiente
+                if ($studente['voto_medio'] >= 6) { ?>
+        <li><?= $studente['nome'] . ' ' . $studente['cognome'] ?> ~ Età: <?= $studente['anni'] ?> ~ Voto medio:
             <?= $studente['voto_medio'] ?></li>
+        <?php } ?>
         <?php } ?>
     </ul>
     <?php } ?>
+
 </body>
 
 </html>
